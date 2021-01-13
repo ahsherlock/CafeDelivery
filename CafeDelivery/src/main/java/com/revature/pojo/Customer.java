7cea2824bee6e2f40bace0e2b16c01dd83447687
will.h <64234764+chh14025@users.noreply.github.com>
2021-01-12 23:07:44 -0500

    Customer, Orders, Menu POJO added, src main resources cfg.xml added (yet tested)

---
+++ CafeDelivery/src/main/java/com/revature/pojo/Customer.java
@@ -0,0 +1,141 @@
+package com.revature.pojo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	
+	@Id
+	@Column(name = "customer_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int customerId;
+	
+	@Column(name="username", nullable = false)
+	private String username;
+	
+	@Column(name="password", nullable = false)
+	private String password;
+	
+	@Column(name="first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name="last_name", nullable = false)
+	private String lastName;
+	
+	@OneToMany(mappedBy = "customerOrder", fetch = FetchType.LAZY)
+	private List<Orders> orderList = new ArrayList<>();
+	
+	
+	
+	public Customer() {
+		super();
+	}
+
+
+
+
+	public Customer(int customerId, String username, String password, String firstName, String lastName) {
+		super();
+		this.customerId = customerId;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+
+
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", username=" + username + ", password=" + password
+				+ ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+	
+	
+
+}
