3d15ef63b2ded3cf286581db984c98d7b8c39832
George Yoo <gcyoosf@gmail.com>
2021-01-15 13:28:58 -0500

    adds js file and adds psuedo code for fetch calls

---
+++ CafeDelivery/src/main/webapp/resources/index.js
@@ -0,0 +1,86 @@
+/** Notes:
+ * Authentication is handled by Form elements directly to the Server.
+ * - Logging in
+ * - Logging Out
+ */
+
+////////////////////
+// FETCH CALLS
+////////////////////
+
+/** Registering as a new Customer
+let data = {
+    username: ?,
+    firstName: ?,
+    lastName: ?,
+    password: ?
+}
+
+fetch('localhost:8080/CafeDelivery/api/customer', {
+    method: 'POST',
+    body: data
+})
+.then(response => {
+
+})
+.then(data => {
+
+})
+*/
+
+/** Retrieving all orders by Customer ID
+fetch('http://localhost:8080/CafeDelivery/api/landing')
+.then(response => {
+
+})
+.then(data => {
+
+})
+*/
+
+/** Retrieving order items by Order ID
+fetch('http://localhost:8080/CafeDelivery/api/landing')
+.then(response => {
+
+})
+.then(data => {
+
+})
+*/
+
+/** Posting a new order
+
+let data = {
+    "orderId": ?,
+    "customerOrder": ?,
+    "item_name": items
+}
+
+let itemsArray = [];
+
+function customerSelectionObjCreator(name, amount) {
+    let tempObj = {
+        "itemName": name,
+        "itemPrice": amount
+    };
+    return tempObj
+}
+
+items.append(customerElectionObjCreator);
+
+fetch('localhost:8080/CafeDelivery/api/order', {
+    method: 'POST',
+    body: data
+})
+.then(response => {
+
+})
+.then(data => {
+
+})
+*/
+
+
+////////////////////
+// DOM Manipulation
+////////////////////
