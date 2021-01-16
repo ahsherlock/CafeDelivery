876a20d45d337c12a14e5bdac471c5aab957104d
George Yoo <gcyoosf@gmail.com>
2021-01-16 07:59:09 -0500

    copies util and hibernate cfg

---
+++ CafeDelivery/src/main/java/com/revature/util/CafeConnection.java
@@ -0,0 +1,25 @@
+package com.revature.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CafeConnection {
+	private static SessionFactory sf = new Configuration().configure("Hibernate.cfg.xml").buildSessionFactory();
+
+	private static Session sesh; // This is our Global Session Object
+
+	public static Session getSession() {
+		if (sesh == null) {
+			return sesh = sf.openSession();
+		}
+
+		return sesh;
+	}
+
+	public static void closeSession() {
+		sesh.close();
+		sesh = null;
+		sf.close();
+	}
+}
